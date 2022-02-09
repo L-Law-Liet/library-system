ba606d394f78dbd01a487af14eb1b2e136ae7f0c
Aliakbar <ali.k@tunelab.net>
2022-02-09 19:19:02 +0500

    model relations

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,10 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'publisher_id'];
+
+    /**
+     * @return BelongsToMany
+     */
+    public function authors(): BelongsToMany
+    {
+        return $this->belongsToMany(Author::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
